b1abdfc9c005970ce3a2d6331ecc752e9f17a395
betselotT <betselottesfa9@gmail.com>
2025-01-21 21:11:27 -0900

    uncommented reports in students list

--- src/Pages/StudentList.jsx
+++ src/Pages/StudentList.jsx
@@ -315,13 +315,13 @@ const StudentsList = () => {
           />
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
         </div>
-        {/* <button
+        <button
           onClick={() => setShowReport(!showReport)}
           className="ml-4 px-4 py-2 bg-[#1a365d] text-white rounded-lg hover:bg-[#234678] transition-colors duration-200 flex items-center"
         >
           <BarChart2 className="mr-2 h-5 w-5" />
           {showReport ? "Hide Report" : "Show Report"}
-        </button> */}
+        </button>
       </div>
 
       {/* Students List */}
@@ -406,7 +406,7 @@ const StudentsList = () => {
         )}
       </div>
 
-      {/* Usage Analysis Report
+      {/* Usage Analysis Report */}
       {showReport && (
         <div className="mt-8 bg-white p-6 rounded-lg shadow-md">
           <h2 className="text-2xl font-semibold mb-4 text-[#1a365d]">
@@ -511,7 +511,7 @@ const StudentsList = () => {
             </div>
           </div>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
